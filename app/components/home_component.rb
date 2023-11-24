5414e242c9aa4c15345abae3162e909e15fa95ff
Josh Clayton <joshua.clayton@gmail.com>
2023-11-24 08:11:32 -0500

    Demonstrate failure with erb_template block

--- app/components/home_component.rb
+++ app/components/home_component.rb
@@ -11,6 +11,11 @@ end
 class HomeComponent < ViewComponent::Base
   using RefineString
 
+  erb_template <<-ERB
+    <p><%= yelled_message %></p>
+    <p><%= @message.yell %></p>
+  ERB
+
   def initialize(message:)
     @message = message
   end
